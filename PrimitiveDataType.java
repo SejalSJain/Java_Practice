1ab4254f83c4165f26c4d1e3a35056d6d9deb001
SejalSJain <jsejal234@gmail.com>
2025-04-10 16:49:14 +0530

    Java Hello file added

---
+++ PrimitiveDataType.java
@@ -0,0 +1,8 @@
+package Java1;
+
+public class PrimitiveDataType {
+  public static void main(String args[]){
+    System.out.println("Primitive int 2 byte");
+    System.out.println("byte, short, int, long, char, boolean, float, double");
+  }
+}
