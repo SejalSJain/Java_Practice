0146f9ee96b7d430fd92247b92302f141640743d
SejalSJain <jsejal234@gmail.com>
2025-04-11 11:45:50 +0530

    New change

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package Java1;
 
 public class Hello {
   public static void main(String args[]){
-    System.out.print("Hello World");
+    System.out.print("this is new java project pushed by using git push --set-upstream origin master command");
   }
 }
