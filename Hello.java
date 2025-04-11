b10b169ab80961b2703174e47dfc6e6f35f7d783
SejalSJain <jsejal234@gmail.com>
2025-04-11 11:47:15 +0530

    New line of code added

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package Java1;
 public class Hello {
   public static void main(String args[]){
     System.out.print("this is new java project pushed by using git push --set-upstream origin master command");
+    System.out.println("New line of code");
   }
 }
